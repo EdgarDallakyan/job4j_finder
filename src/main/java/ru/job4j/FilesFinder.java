c3b9a7d045471d01166cf0097875a12efcd843a2
edgar <edgar_dallakyan@mail.ru>
2024-07-20 15:51:51 +0300

    Тестовое задание. Утилита "поиск файлов". [#783]

--- src/main/java/ru/job4j/FilesFinder.java
+++ src/main/java/ru/job4j/FilesFinder.java
@@ -91,4 +91,5 @@ public class FilesFinder {
         }
         System.out.println(results.size());
     }
+
 }
--- src/main/java/ru/job4j/SearchFiles.java
+++ src/main/java/ru/job4j/SearchFiles.java
@@ -27,4 +27,5 @@ public class SearchFiles extends SimpleFileVisitor<Path> {
         }
         return CONTINUE;
     }
+
 }
